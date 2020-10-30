a3acf80376665a8fb00133c4b335e301190435ce
Ricardo <riicardolivera@gmail.com>
2020-10-30 09:49:27 -0400

    refactor(index) Colocado no padrão wordpress

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-        <?php 
-            echo "Ricardo Maia";
-        ?>
-</body>
-</html>
+<?php get_header(); ?>
+
+<?php if(have_posts()) { while (have_posts(  )) { the_post(); ?> 
+
+    <h1><?php the_title(); ?></h1>
+    <main><?php the_content() ?> </main>
+
+<?php } } ?>
+
+<?php get_footer(); ?>
