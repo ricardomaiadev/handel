15cb3acbac118e002535c7a0449c000984cb3808
Ricardo <riicardolivera@gmail.com>
2020-10-30 17:54:43 -0400

    refactor(home) add slide

--- page-home.php
+++ page-home.php
@@ -2,11 +2,47 @@
 // Template name: Home
 get_header();
 ?>
+<pre>
+<?php
 
-<?php if(have_posts()) { while (have_posts(  )) { the_post(); ?> 
+    $products_slide = wc_get_products([
+        'limit' => 6,
+        'tag' => ['slide'],
+    ]);
 
-    <h1><?php the_title(); ?></h1>
-    <main><?php the_content() ?> </main>
+    function format_products($products) {
+        $products_final = [];
+        foreach($products as $product) {
+            $products_final[] = [
+                'name' => $product->get_name(),
+                'preco' => $product->get_price_html(),
+                'link' => $product->get_permalink(),
+                'img' => wp_get_attachment_image_src($product->get_image_id(), 'slide')[0],
+            ];
+        }
+        return $products_final;
+    }
+
+    $slide = format_products($products_slide);
+
+?>
+</pre>
+<?php if(have_posts()) { while (have_posts(  )) { the_post(); ?>
+    
+<section class="slide-wrapper">
+    <ul class="slide">
+        <?php foreach($slide as $product) { ?>
+        <li class="slide-item">
+            <img src="<?= $product['img']; ?>" alt="<?= $product['name']; ?>">
+            <div class="slide-info">
+                <span class="slide-preco"><?= $product['preco']; ?></span>
+                <h2 class="slide-nome"><?= $product['name']; ?></h2>
+                <a class="slide-link" href="<?= $product['link']; ?>">Ver Produto</a>
+            </div>
+        </li>
+        <?php } ?>
+    </ul>
+</section>
 
 <?php } } ?>
 
