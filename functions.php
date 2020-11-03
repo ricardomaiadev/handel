379da6559ecf73921eb72052dec254a89a39acb2
Ricardo <riicardolivera@gmail.com>
2020-11-03 17:48:36 -0400

    refactor(functions) add function list

--- functions.php
+++ functions.php
@@ -20,13 +20,46 @@ function handel_custom_images() {
 
 }
 
-/* add_action('after_setup_theme', 'handel_custom_images');
+add_action('after_setup_theme', 'handel_custom_images');
 
 
 function handel_loop_shop_per_page() {
-    return 10;
+    return 6;
 }
 
-add_filter('loop_shop_per_page', 'handel_loop_shop_per_page'); */
+add_filter('loop_shop_per_page', 'handel_loop_shop_per_page');
+
+function format_products($products, $img_size ='medium') {
+    $products_final = [];
+    foreach($products as $product) {
+        $products_final[] = [
+            'name' => $product->get_name(),
+            'price' => $product->get_price_html(),
+            'link' => $product->get_permalink(),
+            'img' => wp_get_attachment_image_src($product->get_image_id(), $img_size)[0],
+        ];
+    }
+    return $products_final;
+}
+
+
+function handel_product_list($products) { ?>
+    <ul class="products-list">
+        <?php foreach($products as $product) { ?>
+            <li class="product-item">
+                <a href="<?php $product['link']; ?>">
+                    <div class="product-info">
+                        <img src="<?= $product['img']; ?>" alt="<?= $product['names']; ?>">
+                        <h2><?= $product['name']; ?> - <span><?= $product['price']; ?></span></h2>
+                    </div>
+                    <div class="product-overlay">
+                        <span class="btn-link">Ver Mais</span>
+                    </div>
+                </a>
+            </li>
+        <?php } ?>
+    </ul>
+<?php
+} // Fecha a função handel
 
 ?>
