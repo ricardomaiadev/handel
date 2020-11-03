5dcdf5a889b65d86765058b6f4fa82c5f8ed597a
Ricardo <riicardolivera@gmail.com>
2020-11-03 17:58:19 -0400

    add(arquivos) nova page de arquivos

---
+++ woocommerce/archive-product.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+<?php 
+$products = [];
+    if(have_posts()) { while(have_posts()) { the_post();
+        $products[] = wc_get_product(get_the_ID());
+    } }
+
+$data = [];
+$data['products'] =  format_products($products);
+?>
+
+<div class="container breadcrumb">
+    <?php woocommerce_breadcrumb(['delimiter' => ' > ']) ?> 
+</div>
+
+<article class="container products-archive">
+    <nav class="filtros">
+        <h2>Filtros</h2>
+        <div>
+            <?php 
+               wp_nav_menu([
+                   'menu' => 'categorias-internas'
+                    'menu_class' => 'filtro-cat',
+                    'container' => false,                    
+               ]);
+            ?>
+        </div>
+    </nav>
+    <main>
+        <?php if($data['products'][0]) { ?> 
+        <?php handel_product_list($data['products']); ?>
+        <?= get_the_posts_pagination(); ?>
+        <?php } else { ?>
+            <p>Nenhum resultado para a sua busca.</p>
+        <?php } ?>
+    </main>
+</article>
+
+<?php get_footer(); ?>
