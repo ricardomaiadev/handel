5080870b1dcf4ceb30b834ab15fde11fa4f847dd
Ricardo <riicardolivera@gmail.com>
2020-11-05 18:03:37 -0400

    refactor(index) alterado o titulo e add classe container

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 <?php if(have_posts()) { while (have_posts(  )) { the_post(); ?> 
 
-    <h1><?php the_title(); ?></h1>
-    <main><?php the_content() ?> </main>
+    <h1 class="titulo"><?php the_title(); ?></h1>
+    <main class="container container-index"><?php the_content() ?> </main>
 
 <?php } } ?>
 
